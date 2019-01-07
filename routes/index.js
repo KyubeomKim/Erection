f4f009614bdbd17322136ea1e9ed2af70a5c9859
Kyubeom Kim <sk.kyubeom.kim@gmail.com>
2019-01-07 18:31:04 +0900

    dashboard

--- routes/index.js
+++ routes/index.js
@@ -2,43 +2,58 @@ var express = require('express');
 var router = express.Router();
 
 const XLSX = require("xlsx");
+const XLSX_CALC = require('xlsx-calc');
+var formulajs = require("formulajs");
+XLSX_CALC.import_functions(formulajs)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'zzz' });
 });
 
-router.get("/excel", function(req, res, next) {
+router.get("/dashboard", function(req, res, next) {
     let workbook = XLSX.readFile("./data/happy.xlsx")
-    let worksheet = workbook.Sheets["log"]
-        // console.log(XLSX.utils.sheet_to_json(worksheet))
-    res.render('index', { object: XLSX.utils.sheet_to_json(worksheet) });
+    let worksheetDashboard = workbook.Sheets["Dashboard"]
+    console.log(XLSX.utils.sheet_to_json(worksheetDashboard))
+
+    res.render('dashboard', { params: XLSX.utils.sheet_to_json(worksheetDashboard) });
+});
+
+router.get("/log", function(req, res, next) {
+    let workbook = XLSX.readFile("./data/happy.xlsx")
+    let worksheetLog = workbook.Sheets["log"]
+        // console.log(XLSX.utils.sheet_to_json(worksheetLog))
+    res.render('index', { object: XLSX.utils.sheet_to_json(worksheetLog) });
 });
 
 router.get("/insert", function(req, res, next) {
     let workbook = XLSX.readFile("./data/happy.xlsx")
-    let worksheet = workbook.Sheets["log"]
+    let worksheetLog = workbook.Sheets["log"]
+    let worksheetDashboard = workbook.Sheets["Dashboard"]
 
     var columns = ['A', 'B', 'C', 'D'];
-    var newIndex = parseInt(worksheet['!ref'].split(':')[1].slice(1)) + 1;
-    worksheet['!ref'] = 'A1:D' + newIndex;
+    var newIndex = parseInt(worksheetLog['!ref'].split(':')[1].slice(1)) + 1;
+    worksheetLog['!ref'] = 'A1:D' + newIndex;
 
     for (var i = 0; i < columns.length; i++) {
         if (i == 0) {
-            worksheet[columns[i] + newIndex] = {
+            worksheetLog[columns[i] + newIndex] = {
                 t: 'n',
                 v: newIndex - 1
             }
         } else {
-            worksheet[columns[i] + newIndex] = {
+            worksheetLog[columns[i] + newIndex] = {
                 t: 'n',
                 v: 100
             }
+            worksheetDashboard['B' + (i + 1)].v += 100
+            worksheetDashboard['B7'].v += 100
         }
     }
-    // write to new file
+    XLSX_CALC(workbook)
+        // write to new file
     XLSX.writeFile(workbook, './data/happy.xlsx');
-    res.redirect('/excel')
+    res.redirect('/log')
 });
 
 module.exports = router;
