4861add5f129ea5d3d6dac3b5097ee9f5adf91c9
Kyubeom Kim <sk.kyubeom.kim@gmail.com>
2019-01-08 18:31:08 +0900

    black bug fixed

--- routes/index.js
+++ routes/index.js
@@ -67,10 +67,10 @@ router.post("/insert", function(req, res, next) {
         } else {
             worksheetLog[columns[i] + newIndex] = {
                 t: 'n',
-                v: req.body['player' + (i)] == '' ? 0 : parseInt(req.body['player' + (i)])
+                v: (req.body['player' + (i)] == '' ? 0 : parseInt(req.body['player' + (i)]))
             }
-            worksheetDashboard['B' + (i + 1)].v += req.body['player' + (i)] == '' ? 0 : parseInt(req.body['player' + (i)])
-            worksheetDashboard['B7'].v += req.body['player' + (i)] == '' ? 0 : parseInt(req.body['player' + (i)])
+            worksheetDashboard['B' + (i + 1)].v += (req.body['player' + (i)] == '' ? 0 : parseInt(req.body['player' + (i)]))
+            worksheetDashboard['B7'].v += (req.body['player' + (i)] == '' ? 0 : parseInt(req.body['player' + (i)]))
         }
     }
     XLSX_CALC(workbook)
@@ -83,7 +83,7 @@ router.post("/totalupdate", function(req, res, next) {
     let workbook = XLSX.readFile("./data/" + filename)
     let worksheetDashboard = workbook.Sheets["Dashboard"]
 
-    worksheetDashboard['B7'].v = req.body['total'] == '' ? 0 : parseInt(req.body['total'])
+    worksheetDashboard['B7'].v = (req.body['total'] == '' ? 0 : parseInt(req.body['total']))
     XLSX_CALC(workbook)
         // write to new file
     XLSX.writeFile(workbook, './data/happy.xlsx');
@@ -97,7 +97,7 @@ router.post("/calculate", function(req, res, next) {
 
     for (var i = 2; i < 5; i++) {
         worksheetTotal["B" + i].v += worksheetDashboard["E" + i].v
-        worksheetTotal["C" + i].v += worksheetDashboard["D" + i].v - req.body['player' + (i - 2)] == '' ? 0 : parseInt(req.body['player' + (i - 2)]);
+        worksheetTotal["C" + i].v += worksheetDashboard["D" + i].v - (req.body['player' + (i - 2)] == '' ? 0 : parseInt(req.body['player' + (i - 2)]));
         worksheetDashboard["B" + i].v = 0
     }
     worksheetDashboard['B7'].v = 0
